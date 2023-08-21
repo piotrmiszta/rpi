ab0094c749ff726e244497ba490de3c7680a259f
Piotr Miszta <miszta.piotr.pm@gmail.com>
2023-08-21 10:57:34 +0200

    [ADD] Handling request from client
    Description
    Handling info requests

---
+++ server/job_info/include/handle_job_info_device_list.h
@@ -0,0 +1,7 @@
+#ifndef HANDLE_JOB_INFO_DEVICE_LIST_H_
+#define HANDLE_JOB_INFO_DEVICE_LIST_H_
+#include "messages.h"
+
+MessageS* handle_job_info_device_list(const MessageS* const msg);
+
+#endif //HANDLE_JOB_INFO_DEVICE_LIST_H_
---
+++ server/job_info/src/handle_job_info_device_list.c
@@ -0,0 +1,7 @@
+#include "handle_job_info_device_list.h"
+#include "devices.h"
+
+MessageS* handle_job_info_device_list(const MessageS* const msg) {
+    (void)msg;
+    return NULL;
+}
